e9afbc649c44b505584a46e1cfa4b0bf4493298e
david travailleur <travailleur.david@gmail.com>
2020-12-02 17:18:14 +0100

    Valid seeds + fix + Add row expected level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_144241) do
+ActiveRecord::Schema.define(version: 2020_12_02_161055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_144241) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "owner_id", null: false
+    t.string "expected_level"
     t.index ["owner_id"], name: "index_events_on_owner_id"
   end
 
