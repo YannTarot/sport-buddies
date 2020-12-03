8eca8b4fda3a37eba08538462b2152e435ca2e81
Yann Tarot <yann_tarot@yahoo.fr>
2020-12-03 11:05:00 +0100

    updating css button component

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_12_02_161055) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +46,9 @@ ActiveRecord::Schema.define(version: 2020_12_02_161055) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "owner_id", null: false
-    t.string "expected_level"
     t.float "latitude"
     t.float "longitude"
-
+    t.string "expected_level"
     t.index ["owner_id"], name: "index_events_on_owner_id"
   end
 
