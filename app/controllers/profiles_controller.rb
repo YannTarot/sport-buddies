4354bd0816f80e3e74360291e469725348093be2
nicolas blum <nblumferracci@gmail.com>
2020-12-07 12:15:55 +0100

    implement past events

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,12 @@
 class ProfilesController < ApplicationController
   def show
+    @confirmed_participations = current_user.participations.where(status: "confirmed")
+    @past_events = current_user.joined_events.where(id: @confirmed_participations.pluck(:event_id)).where("starts_at < ?", Time.now)
   end
 
   def update
   end
+
 end
 
 private
