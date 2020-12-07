9e8bc7e6ee55d585bc177c0d0c663e784f2e31cd
Yann Tarot <yann_tarot@yahoo.fr>
2020-12-07 14:16:42 +0100

    Displaying dynamic html on profile

---
+++ app/javascript/components/profile-navbar.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    const first = document.getElementById("friendlist");
+    first.outerHTML = "";
+
+    const second = document.getElementById("pending-request");
+    second.innerHTML = "";
+  });
