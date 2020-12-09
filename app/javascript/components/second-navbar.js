c7900b24cc29a7956eb6b26507410e3560621014
Yann Tarot <yann_tarot@yahoo.fr>
2020-12-09 16:44:19 +0100

    Fixing footer and grid in homepage

---
+++ app/javascript/components/second-navbar.js
@@ -0,0 +1,18 @@
+export const initNavbarAnimation = () => {
+  $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+  });
+
+
+  $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+          $('.nav').addClass('affix');
+          console.log("OK");
+      }
+  });
+
+};
